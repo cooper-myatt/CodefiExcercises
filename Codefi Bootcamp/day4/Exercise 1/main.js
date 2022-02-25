9284e0ffa1f5b778ec606c29f80f64d2b90d5715
cooper-myatt <66887110+cooper-myatt@users.noreply.github.com>
2022-02-24 20:28:52 -0600

    Class 4
    Updated class files from day 4

---
+++ Codefi Bootcamp/day4/Exercise 1/main.js
@@ -0,0 +1,17 @@
+//Variables
+
+cButton = document.querySelector(".colorButton");
+
+//Functions
+function randomColor(){
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    return(`#${randomColor}`)
+}
+
+function changeColor() {
+    document.body.style.backgroundColor = randomColor();
+    cButton.style.color = randomColor();
+}
+
+//Listeners
+cButton.addEventListener('click', changeColor);
---
+++ Codefi Bootcamp/day4/Exercise 2/main.js
@@ -0,0 +1,31 @@
+//Create a function that returns the number of true values in an array.
+
+//Create an array
+
+//Format message for countTF
+function messageOutput(t,f){
+    return `${t} True Values in the Array \n${f} False Values in the Array`
+}
+
+//count number of true values in array
+function countTF(arr){
+let trueCount = 0
+let falseCount = 0
+if (arr.length !== 0){
+    arr.forEach(element => {
+    element === true ? trueCount++ : falseCount++;
+    message = messageOutput(trueCount, falseCount);
+});
+    return message
+}
+else{
+    return "Please enter a non-empty array";
+}
+
+}
+
+//display result
+let array = [true, false, false, true, false];
+console.log(countTF(array));
+array = [];
+console.log(countTF(array));
