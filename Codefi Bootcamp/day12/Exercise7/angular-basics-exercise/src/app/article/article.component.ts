79f163401dcb5e2e4d5bdfeaf8ae634e478fa30f
cooper-myatt <66887110+cooper-myatt@users.noreply.github.com>
2022-03-24 19:21:59 -0600

    3/24/2022-2

--- Codefi Bootcamp/day12/Exercise7/angular-basics-exercise/src/app/article/article.component.ts
+++ Codefi Bootcamp/day12/Exercise7/angular-basics-exercise/src/app/article/article.component.ts
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
 })
 export class ArticleComponent {
   title: string = "Whatever you want";
-  content: string = "Some content goes here"
+  content: string = "Some content goes here";
+  isTechRelated: boolean = true;
 }
